dec78e643f229dc285a7e6be35cb762ce5230c9b
Hyejin Shin <enthusiastic.shj@gmail.com>
2018-01-22 15:54:58 +0900

    게시글을 파이어베이스에서 불러오기

--- src/components/ArticleListScreen.js
+++ src/components/ArticleListScreen.js
@@ -1,14 +1,58 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
+import {Table} from 'semantic-ui-react';
 import NavBar from './NavBar';
 //import * as firebase from 'firebase';
 
-export default class Article extends Component {
+// const mockData = [
+//   {
+//     articleId: '-LB1', //push했을 때 자동생성되는 식별자
+//     author: '신혜진',
+//     title: '게시글 제목',
+//     createdAt: '2018-01-20'
+//   },
+//   {
+//     articleId: '-LB2',
+//     author: '신혜진',
+//     title: '게시글 제목2',
+//     createdAt: '2018-01-20'
+//   }
+// ];
+const ArticleItemRow = styled(Table.Row)`
+  &:hover {
+    background:#daa357;
+    cursor:pointer;
+    color:#fff;
+  }
+`;
+export default class ArticleListScreen extends Component {
   render() {
-    const {uid, onNickNameClick} = this.props; //분해대입
+    const {nickName, onNickNameClick, articleArr} = this.props; //분해대입
     return (
       <div>
-        <NavBar uid={uid} onNickNameClick={onNickNameClick}/>
-        게시글 목록
+        <NavBar nickName={nickName} onNickNameClick={onNickNameClick}/>
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>작성자</Table.HeaderCell>
+              <Table.HeaderCell>제목</Table.HeaderCell>
+              <Table.HeaderCell>작성일</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {
+              Array.isArray(articleArr) && articleArr.length > 0
+                ? articleArr.map(({articleId, title, author, createdAt}) => (
+                  <ArticleItemRow key={articleId}>
+                    <Table.Cell>{author}</Table.Cell>
+                    <Table.Cell>{title}</Table.Cell>
+                    <Table.Cell>{createdAt}</Table.Cell>
+                  </ArticleItemRow>
+                ))
+                : '게시글이 없습니다.'
+            }
+          </Table.Body>
+        </Table>
       </div>
     )
   }
--- src/components/BBS.js
+++ src/components/BBS.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import * as firebase from 'firebase';
 import Login from './LoginScreen';
-import Article from './ArticleListScreen';
-import Account from './AccountScreen';
+import ArticleListScreen from './ArticleListScreen';
+import AccountScreen from './AccountScreen';
 
 export default class BBS extends Component {
   state = {
@@ -18,20 +18,21 @@ export default class BBS extends Component {
       messagingSenderId: "189822287220"
     };
     firebase.initializeApp(config);
-    firebase.auth().onAuthStateChanged( user => {  //function 키워드를 화살표함수로 변경, this.setState .. this 때문
+    firebase.auth().onAuthStateChanged( async user => {  //function 키워드를 화살표함수로 변경, this.setState .. this 때문
       if (user) {
+        const snapshot = await firebase.database().ref(`users/${user.uid}/nickName`).once('value');
         // User is signed in.
-        this.setState(prevState => { //이전상태 참조하는 '함수'형 코드
-          return {
+        this.setState({
             page: 'list',
-            uid: user.uid
-          }
+            uid: user.uid,
+            nickName: snapshot.val()
         });
+
+        this.fetchArticles();
+
       } else {
-        this.setState(prevState => {
-          return {
+        this.setState({
             page: 'login'
-          }
         });
       }
     });
@@ -44,16 +45,77 @@ export default class BBS extends Component {
       }
     })
   }
+
+  //firebase에 저장요청
+  saveNickName = async nickName => {
+    const {uid} =  this.state;
+    await firebase.database().ref(`users/${uid}/nickName`).set(nickName);
+    this.setState({
+      nickName: nickName,
+      page: 'list'
+    });
+  }
+  fetchArticles = async () => {
+    //
+    const nickNameSnapshot = await firebase.database().ref(`users/7uaAewMGqJPvfbo9KQ1XnrxfLFm1/nickName`).once('value');
+    const nickName = nickNameSnapshot.val();
+
+    //
+    const snapshot = await firebase.database().ref('articles').once('value');
+    const articlesObj = snapshot.val();
+    if(articlesObj == null){
+      this.setState({
+        articles:null
+      });
+    }else{
+      const articles = Object.entries(articlesObj).map(([articleId,articleItem]) => {
+        return {
+          ...articleItem,
+          articleId
+        }
+      });
+      const uidSet = new Set(articles.map(({uid}) => uid));
+      const uidObj = {};
+      const ps = Array.from(uidSet).map(async uid => {
+        const snapshot = await firebase.database().ref(`users/${uid}/nickName`).once('value');
+        const nickName = snapshot.val();
+        return [uid, nickName];
+      })
+      const pairArr = await Promise.all(ps)
+      for(const [uid, nickName] of pairArr) { //분해대입
+        uidObj[uid] = nickName;
+      }
+
+      // for(const uid of uidArr){
+      //   const nickNameSnapshot = await firebase.database().ref(`users/${uid}/nickName`).once('value');
+      //   const nickName = nickNameSnapshot.val();
+      //   uidObj[uid] = nickName;
+      // }
+      articles.forEach(article => {
+        article.author = uidObj[article.uid];
+      });
+      this.setState({
+        articles
+      });
+    }
+  }
   render() {
+    const {nickName, uid, articles} = this.state;
     return (
       <div>
         {
           this.state.page === 'login'
             ? <Login />
             : this.state.page === 'list'
-            ? <Article onNickNameClick={this.pageToAccount}uid={this.state.uid}/>
+            ? <ArticleListScreen
+                onNickNameClick={this.pageToAccount}
+                nickName={nickName || uid}
+                articleArr={articles} />
             : this.state.page === 'account'
-            ? <Account />
+            ? <AccountScreen
+                onNickNameClick={this.pageToAccount}
+                nickName={nickName || uid}
+                onNickNameSubmit={this.saveNickName} />
             : null
         }
       </div>
